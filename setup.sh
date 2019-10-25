01dc315acf5ba7239f647af967a37a64b7a99097
mepley <mepley@breccia>
2019-10-25 12:31:40 -0400

    Updates to layout, more content

---
+++ setup.sh
@@ -0,0 +1,7 @@
+oc login mepley-demo.redhatgov.io -u admin -p Redhat1!
+
+oc get project status || oc new-project status
+
+oc new-app https://github.com/michaelepley/ocktoberfest.git --context-dir=status
+
+oc expose svc/ocktoberfest
--- status/index.php
+++ status/index.php
@@ -7,28 +7,72 @@
 	  src: url('fonts/bertholdr-mainzer-fraktur/Bertholdr-Mainzer-Fraktur.ttf');
 	}
     table, th, td {
-      border: 1px solid black;
+      border: 0px solid black;
       border-collapse: collapse;
     } 
     th, td {
       padding: 5px;
-      text-align: left;
+      text-align: center;
+      font-family: sans-serif;
+      border:0;
     }
 	body {
-	  background-image: url("images/sbavarian-pattern_640.png");
+	  background-image: url("images/bavarian-pattern_640.png");
 	  background-repeat: repeat;
-	}
-	body {
-	  
 	}
   </style>
 </head>
 <body>
-<h1 style="font-family: Bavarian;">Mike and Cathy's Ocktoberfest</h1>
 <br/>
-<div style="width: 800px;
-	height: 600px;
-	background-color: cream;">
+<h1 style="text-align:center; font-family: Bavarian;">Mike and Cathy's Ocktoberfest</h1>
+<div >
+<table style="align: center;   margin: auto; width: 80%;">
+<tr>
+<td>
+<h2>October 27, 2pm-10pm</h2>
+</td>
+<td>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</td>
+<td>
+<h2>4407 6th St South, Arlington VA</h2>
+</td>
+</tr>
+</table>
+</div>
+<br/>
+<div style="
+  align: center;
+  margin: auto;
+    width: 80%;
+	height: 80px;
+	background-color: #00aa00;
+	justify-content: center;
+	border: darkgreen 5px solid;
+	font-size: 20px;
+	font-family: sans-serif;
+	padding: 10px;
+	text-align: center;
+	">
+<p>
+Weather Update: Unfortunately, the weather forecast is not looking great for Sunday with showers (potentially heavy at times) predicted. We are still A GO at the moment, but might move our activities indoors.
+</p>
+</div>
+<div style="height: 40px;">
+</div>
+<div style="
+  align: center;
+  margin: auto;
+    width: 80%;
+	height: 400px;
+	background-color: #fffdd0;
+	justify-content: center;
+	border: darkblue 5px solid;
+	font-size: 20px;
+	font-family: sans-serif;
+	padding: 30px;
+	text-align: center;
+	">
 <p>
 Grab your 1L stein, put on your favorite lederhosen, and bring the kids. Please join us, our friends, family, coworkers, and neighbors for our quasi-annual ocktoberfest get together. We realize its late in the year for "real" ocktoberfest -- but its really just a good excuse!
 </p>
@@ -42,16 +86,17 @@ While we will have some food and drinks, please feel free to bring something (si
 </p>
 
 <p>
-And for the uninitiated, you'll want to <a href="https://www.youtube.com/watch?v=zsbyUdmd8ys" review the basics on how to oktoberfest</a>.
+And for the uninitiated, you'll want to <a href="https://www.youtube.com/watch?v=zsbyUdmd8ys"> review the basics on how to oktoberfest</a>.
 </p>
 </div>
 <?php
+$refresh=30;
 // <!-- Automatically refresh page every few seconds, if the refresh request parameter is provided -->  
 if (isset($_GET["refresh"])) {
 	$refresh=$_GET["refresh"]; 
 	$url=$_SERVER['REQUEST_URI'];
-	header("Refresh: $refresh; URL=$url");
 }
+header("Refresh: $refresh; URL=$url");
 
 error_reporting(E_ERROR);
 
@@ -59,4 +104,3 @@ error_reporting(E_ERROR);
 </div>
  </body>
 </html>
-
